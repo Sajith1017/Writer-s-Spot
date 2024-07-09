9ff51fd81f83bd2c1ec155a9b78a9d63bbc2fc65
Sajith1017 <sajithahmed1017@gmail.com>
2024-07-09 14:28:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+var over = document.querySelector(".overley")
+var pop= document.querySelector(".popup")
+add.addEventListener("click",function()
+{over.style.display="block"
+    pop.style.display="block"
+})
+
+box5.addEventListener("click",function(event)
+{event.preventDefault()
+    over.style.display="none"
+    pop.style.display="none"
+})
+
+
+var con = document.querySelector(".container")
+box4.addEventListener("click",function(event){
+    event.preventDefault()
+    var div = document.createElement("div")
+    div.setAttribute("class","bookcontainer")
+    div.innerHTML=`<h2>${box1.value}</h2>
+    <h5>${box2.value}</h5>
+    <P>${box3.value}</P>
+    <button onclick="erase(event)">Delete</button>`
+    con.append(div)
+    over.style.display="none"
+    pop.style.display="none"
+})
+
+function erase(event)
+{event.target.parentElement.remove()}
+
